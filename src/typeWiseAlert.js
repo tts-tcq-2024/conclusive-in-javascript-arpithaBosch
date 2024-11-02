b060e282049dbe4ae77cc133ed73f63dbf12219e
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-11-02 14:21:31 +0530

    Update typeWiseAlert.js

--- src/typeWiseAlert.js
+++ src/typeWiseAlert.js
@@ -1,8 +1,5 @@
-// src/typeWiseAlert.js
-
 const { classifyTemperatureBreach } = require('./temperatureClassification');
 
-// Export only the functions that are used for testing purposes
 module.exports = {
     classifyTemperatureBreach,
 };
