210a12e7dbbd32caa8250cd8c773467f10ec4dde
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-11-02 16:59:33 +0530

    Update typeWiseAlert.test.js

--- test/typeWiseAlert.test.js
+++ test/typeWiseAlert.test.js
@@ -9,18 +9,30 @@ const sinon = require("sinon");
 describe("Temperature Classification and Alert Tests", () => {
   describe("Breach Status Tests", () => {
     it('TC001: should return "TOO_LOW" for temperature below lower limit for PASSIVE_COOLING', () => {
-      expect(classifyTemperatureBreach("PASSIVE_COOLING", -5)).to.equal("TOO_LOW");
-      expect(classifyTemperatureBreach("PASSIVE_COOLING", -10)).to.equal("TOO_LOW");
+      expect(classifyTemperatureBreach("PASSIVE_COOLING", -5)).to.equal(
+        "TOO_LOW"
+      );
+      expect(classifyTemperatureBreach("PASSIVE_COOLING", -10)).to.equal(
+        "TOO_LOW"
+      );
     });
 
     it('TC002: should return "TOO_HIGH" for temperature above upper limit for MED_ACTIVE_COOLING', () => {
-      expect(classifyTemperatureBreach("MED_ACTIVE_COOLING", 50)).to.equal("TOO_HIGH");
-      expect(classifyTemperatureBreach("HI_ACTIVE_COOLING", 46)).to.equal("TOO_HIGH");
+      expect(classifyTemperatureBreach("MED_ACTIVE_COOLING", 50)).to.equal(
+        "TOO_HIGH"
+      );
+      expect(classifyTemperatureBreach("HI_ACTIVE_COOLING", 46)).to.equal(
+        "TOO_HIGH"
+      );
     });
 
     it('TC003: should return "NORMAL" for temperature within limits', () => {
-      expect(classifyTemperatureBreach("PASSIVE_COOLING", 20)).to.equal("NORMAL");
-      expect(classifyTemperatureBreach("MED_ACTIVE_COOLING", 30)).to.equal("NORMAL");
+      expect(classifyTemperatureBreach("PASSIVE_COOLING", 20)).to.equal(
+        "NORMAL"
+      );
+      expect(classifyTemperatureBreach("MED_ACTIVE_COOLING", 30)).to.equal(
+        "NORMAL"
+      );
     });
   });
 
@@ -41,19 +53,30 @@ describe("Temperature Classification and Alert Tests", () => {
     });
 
     it("TC005: should throw an error for unknown cooling types", () => {
-      expect(() => getTemperatureLimits("UNKNOWN_COOLING")).to.throw(Error, "Unknown cooling type");
+      expect(() => getTemperatureLimits("UNKNOWN_COOLING")).to.throw(
+        Error,
+        "Unknown cooling type"
+      );
     });
   });
 
   describe("Classify Temperature Breach Tests", () => {
     it("TC006: should classify temperature breach for PASSIVE_COOLING", () => {
-      expect(classifyTemperatureBreach("PASSIVE_COOLING", 10)).to.equal("NORMAL");
-      expect(classifyTemperatureBreach("PASSIVE_COOLING", -5)).to.equal("TOO_LOW");
+      expect(classifyTemperatureBreach("PASSIVE_COOLING", 10)).to.equal(
+        "NORMAL"
+      );
+      expect(classifyTemperatureBreach("PASSIVE_COOLING", -5)).to.equal(
+        "TOO_LOW"
+      );
     });
 
     it("TC007: should classify temperature breach for HI_ACTIVE_COOLING", () => {
-      expect(classifyTemperatureBreach("HI_ACTIVE_COOLING", 50)).to.equal("TOO_HIGH");
-      expect(classifyTemperatureBreach("HI_ACTIVE_COOLING", 10)).to.equal("NORMAL");
+      expect(classifyTemperatureBreach("HI_ACTIVE_COOLING", 50)).to.equal(
+        "TOO_HIGH"
+      );
+      expect(classifyTemperatureBreach("HI_ACTIVE_COOLING", 10)).to.equal(
+        "NORMAL"
+      );
     });
   });
 
@@ -82,7 +105,11 @@ describe("Temperature Classification and Alert Tests", () => {
 
     it("TC010: should send email alert for temperature breach", () => {
       checkAndAlert("TO_EMAIL", { coolingType: "HI_ACTIVE_COOLING" }, 50);
-      expect(consoleLogStub.calledWith("To: a.b@c.com\nHi, the temperature is too high")).to.be.true;
+      expect(
+        consoleLogStub.calledWith(
+          "To: a.b@c.com\nHi, the temperature is too high"
+        )
+      ).to.be.true;
     });
   });
 });
