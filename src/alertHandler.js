6fa2c30a52d91fdad1c0fe2f73edeec34e8c748c
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-11-02 14:22:52 +0530

    Create alertHandler.js

---
+++ src/alertHandler.js
@@ -0,0 +1,38 @@
+const { classifyTemperatureBreach } = require('./temperatureClassification');
+
+// Checks for temperature breaches and sends alerts based on the specified target.
+function checkAndAlert(alertTarget, batteryChar, temperatureInC) {
+    const breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+    if (alertTarget === 'TO_CONTROLLER') {
+        sendToController(breachType);
+    } else if (alertTarget === 'TO_EMAIL') {
+        sendToEmail(breachType);
+    } else {
+        throw new Error('Unknown alert target');
+    }
+}
+
+// Sends the breach status to the controller.
+function sendToController(breachType) {
+    const header = 0xfeed;
+    console.log(`${header}, ${breachType}`);
+}
+
+// Sends an email alert based on the breach type.
+function sendToEmail(breachType) {
+    const recipient = 'a.b@c.com';
+    let message;
+    // Constructing the message based on the breach type
+    if (breachType === 'TOO_LOW') {
+        message = 'Hi, the temperature is too low';
+    } else if (breachType === 'TOO_HIGH') {
+        message = 'Hi, the temperature is too high';
+    } else {
+        message = 'Hi, the temperature is normal'; // For completeness
+    }
+    console.log(`To: ${recipient}\n${message}`);
+}
+
+module.exports = {
+    checkAndAlert
+};
