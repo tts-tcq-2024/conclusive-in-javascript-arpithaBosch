b32ade19148f5040ef55e22791109f849ea0d493
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-11-02 14:19:43 +0530

    Update and rename typewisealert.js to typeWiseAlert.js

---
+++ src/typeWiseAlert.js
@@ -0,0 +1,8 @@
+// src/typeWiseAlert.js
+
+const { classifyTemperatureBreach } = require('./temperatureClassification');
+
+// Export only the functions that are used for testing purposes
+module.exports = {
+    classifyTemperatureBreach,
+};
